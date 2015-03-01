cef3b2bd4c3f60cba5cf979036edebe6557402fc
GalinaSazonova <galka.saz@yandex.ru>
2015-03-02 00:33:56 +0300

    5

---
+++ 27.02.2015/ArithmTree/ArithmTree/Program.fs
@@ -0,0 +1,18 @@
+﻿type Sign = char
+type Number = int
+
+type Tree =
+| Tip of Number
+| Tree of Sign * Tree * Tree
+
+let rec countTree tree =
+    match tree with
+    | Tree('+', left, right) -> (countTree left) + (countTree right)
+    | Tree('-', left, right) -> (countTree left) - (countTree right)
+    | Tree('*', left, right) -> (countTree left) * (countTree right)
+    | Tree('/', left, right) -> (countTree left) / (countTree right)
+    | Tip value -> value
+    | _ -> 0
+    
+let tree = Tree('+', Tree('*', Tip 2, Tree('-', Tip 10, Tip 2)), Tip 4)
+(countTree tree) |> printf ("Result = %A ")
