5c115b81dc343ca18a33150bb457272f9248c864
GalinaSazonova <galka.saz@yandex.ru>
2015-03-09 17:39:43 +0300

    4hw 1&2

---
+++ 6.03.2015/Brackets/Brackets/Program.fs
@@ -0,0 +1,11 @@
+﻿let bracketBalance list =
+    let openB = List.filter (fun x -> x = '(' || x = '{' || x = '[') list
+    let closeB = List.filter (fun x -> x = ')' || x = '}' || x = ']') list
+    let revcloseB = List.fold (fun acc elem -> elem::acc) [] closeB
+    if (openB.Length = revcloseB.Length) then
+        List.fold2 (fun acc x y -> if (x = '(' && y = ')') || (x = '[' && y = ']') || (x = '{' && y = '}') then acc else false ) true openB revcloseB
+    else false
+printfn ("Please, enter line of brackets")
+let str = System.Console.ReadLine()
+let strInArray = List.ofArray ((string str).ToCharArray())
+(bracketBalance strInArray) |> printfn ("Brackets in right order: %b")
---
+++ 6.03.2015/PointFreeStyleYo/PointFreeStyleYo/Program.fs
@@ -0,0 +1,17 @@
+﻿let func0 x l = List.map (fun y -> y * x) l
+let func1 x : (int)List -> (int)List = List.map (fun y -> y * x)
+let func2 : int -> (int)List -> (int)List = (fun y -> (*) y) >> List.map
+let func3 : int -> (int)List -> (int)List = ((*)) >> List.map
+
+printf "Origin "
+(func0 2 [1;2;3]) |> List.iter (printf"%d ")
+printfn ""
+printf "1 step "
+(func1 2 [1;2;3]) |> List.iter (printf"%d ")
+printfn ""
+printf "2 step "
+(func2 2 [1;2;3]) |> List.iter (printf"%d ")
+printfn ""
+printf "3 step "
+(func3 2 [1;2;3]) |> List.iter (printf"%d ")
+printfn ""
