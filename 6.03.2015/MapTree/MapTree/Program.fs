1b5dee70b77c79686fe044470bec2708b97ff924
GalinaSazonova <galka.saz@yandex.ru>
2015-03-13 18:45:06 +0300

    5з

---
+++ 6.03.2015/MapTree/MapTree/Program.fs
@@ -0,0 +1,9 @@
+﻿type Tree<'a> =
+    | Tip of 'a
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    
+
+let rec mapForTree tree func =
+    match tree with
+    | Tip t -> Tip (func t)
+    | Tree (a, l, r) -> Tree (func a, mapForTree l func, mapForTree r func)
