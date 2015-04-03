4a8f3589facfdd7281433dc93190b3067dd9c743
GalinaSazonova <galka.saz@yandex.ru>
2015-04-03 12:49:12 +0300

    3 meh, mozno ne smotret'

---
+++ кр1/3/3/Program.fs
@@ -0,0 +1,13 @@
+﻿
+type QueuePrior (Element:int, Priority1:int) =
+    let queue:List<QueuePrior> = []
+    member el.Key = Element
+    member el.Priority = Priority1
+    member el.Add =
+        if (queue |> List.fold(fun acc x -> if (x.Priority < el.Priority) then acc + 1 else acc ) 0 > 0) && (queue.Head.Priority < el.Priority)then
+            QueuePrior(Element = el.Key, Priority1 = el.Priority)::queue
+        else
+            queue |> List.append([QueuePrior(Element = el.Key, Priority1 = el.Priority)])
+    member el.Get =
+        //if (queue.IsEmpty) then
+           
