93f7da0a2af905c1e10686ee87f4c2f95828fb5c
GalinaSazonova <galka.saz@yandex.ru>
2015-04-24 14:53:59 +0300

    2z

---
+++ 6.03.2015/Derivative/Derivative/Program.fs
@@ -0,0 +1,58 @@
+﻿type Const = int
+type variable = char
+type Sign = char
+type Polynomial =
+    | Constant of Const
+    | Variable of variable
+    | PolynomialExr of Polynomial * Sign * Polynomial
+
+let rec countDer expression =
+    match expression with
+    | PolynomialExr(l, sign, r) -> 
+        match sign with
+        |'+'|'-' -> PolynomialExr(countDer l, sign, countDer r)
+        |'*' -> PolynomialExr(PolynomialExr(countDer l, sign, r), '+', PolynomialExr(l, sign, countDer r))
+        |'/' -> PolynomialExr(PolynomialExr(PolynomialExr(countDer l, '*', r), '-', PolynomialExr(l, '*', countDer r)), sign, PolynomialExr(r, '*', r))
+        | _ -> Constant 0
+    | Variable _ -> Constant 1
+    | Constant _ -> Constant 0
+
+let rec simplify expression =
+    match expression with
+    | Variable a -> Variable a
+    | Constant b -> Constant b
+    | PolynomialExr(l, sign, r) -> 
+                    let left = simplify l
+                    let right = simplify r
+
+                    match left with
+                    | Constant a -> 
+                        match right with
+                        | Constant b -> 
+                                match sign with
+                                |'+' -> Constant (a + b)
+                                |'-' -> Constant (a - b)
+                                |'*' -> Constant (a * b)
+                                |'/' -> Constant (a / b)
+                                | _ -> Constant 0
+                        | _ -> 
+                            if (a = 1 && sign = '*') || (a = 0 && (sign = '-' || sign = '+')) then
+                                right
+                            elif (a = 0 && (sign = '*' || sign = '/')) then
+                                Constant 0
+                            else
+                                PolynomialExr(left, sign, right)
+                    | _ ->
+                        match right with
+                        | Constant r -> 
+                            if r = 0 && sign = '*' then
+                                Constant 0
+                            elif (r = 1 && (sign = '*' || sign = '/')) || (r = 0 && (sign = '-' || sign = '+')) then
+                                left
+                            else PolynomialExr(left, sign, right)
+                        | _ -> PolynomialExr(left, sign, right)
+
+
+
+(*let tree = PolynomialExr(PolynomialExr(Variable 'x', '*', Variable 'x'), '+', PolynomialExr(Constant 1, '*', Variable 'x'))
+simplify (countDer tree)*)
---
+++ кр1/perep2/perep2/Program.fs
@@ -0,0 +1,25 @@
+﻿let isPalin (number:int) =
+    let rec revList acPrevList =
+       List.fold (fun acc elem -> elem::acc) [] acPrevList
+    let directOrder = List.ofArray ((string (number.ToString())).ToCharArray())
+    let backOrder = revList directOrder
+    directOrder = backOrder
+
+
+let findMax =
+    let rec find number1 number2 max =
+        let rec find2 number1 number2 max =
+            if (number1 * number2 > max) && (isPalin (number1 * number2)) then
+                number1 * number2
+            elif number2 > 0 then
+                find2 number1 (number2 - 1) max
+            else max
+        let t = find2 number1 number2 max
+        if t > max then
+            find (number1 - 1) number2 t
+        elif number1 > 0 then
+            find (number1 - 1) number2 max
+        else 
+            max
+    find 999 999 -1
+    //answer 906609
