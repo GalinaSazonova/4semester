d40b2ed5c3611be47ac60536777b1f79f41fc074
GalinaSazonova <galka.saz@yandex.ru>
2015-04-24 15:03:38 +0300

    3z

---
+++ кр1/perep3/perep3/Program.fs
@@ -0,0 +1,16 @@
+﻿type Stack() =
+    let mutable list = []
+
+    member v.Push el = 
+        list <- el::list
+    
+    member v.IsEmpty = 
+        list =[]
+
+    member v.Pop =
+        match list with
+        |[] -> failwith ("Stack is empty")
+        |_ ->
+            let tmp = list.Head
+            list <- list.Tail
+            tmp
